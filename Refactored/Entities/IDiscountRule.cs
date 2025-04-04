f8d60b738195c0117ae1a5db95c6abd1b2754717
Dagsa Adrix Llou (External) <adrix.dagsa@betterask.erni>
2025-04-04 17:07:49 +0800

    new commit

--- Refactored/Entities/IDiscountRule.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace CodeSmellsCodeExercise.Entities;
-
-public interface IDiscountRule
-{
-    bool IsApplicable(double totalPrice);
-    double ApplyDiscount(double totalPrice);
-}
