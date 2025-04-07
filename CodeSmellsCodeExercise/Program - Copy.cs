bd5a5ba6e5949dad331b341fe41612b397ab6e91
Jardeleza Sophia Gabrielle (External) <sophia.jardeleza@betterask.erni>
2025-04-07 09:25:17 +0800

    add

--- CodeSmellsCodeExercise/Program - Copy.cs
+++ CodeSmellsCodeExercise/Program - Copy.cs
@@ -9,53 +9,60 @@ namespace CodeSmellsCodeExercise
         static void Main(string[] args)
         {
             Customer customer = new Customer();
-            Validation validate = new Validation();
-            Database database = new Database();
             OrderProcessor orderProcessor = new OrderProcessor();
-            DiscountPrice discountPrice = new DiscountPrice();
-            DisplayInfo displayInfo = new DisplayInfo();
+            Database database = new Database();
 
             try
             {
-                customer.GetCustomerDetails("John Doe", "California");
-                validate.ValidateCustomerDetails(customer.customerName, customer.customerAddress);
-                
+                customer.CustomerDetails("Jane Doe", "California");
                 orderProcessor.AddOrder("Laptop", 1);
-                orderProcessor.AddOrder("Towel", 1);
-                
-                double price = orderProcessor.ProcessOrder();
-                
-                validate.ValidateOrder(price);
-                
-                discountPrice.GetDiscount(price);
-                
-                displayInfo.display(customer, price);
-                
+                orderProcessor.AddOrder("Phone", 2);
+                orderProcessor.ProcessOrder();
                 database.SaveOrder(customer, orderProcessor);
-                
-                Console.ReadLine();
+
+
+
+                customer.CustomerDetails("John Doe", "New York");
+                orderProcessor.AddOrder("Tablet", 1);
+                orderProcessor.AddOrder("Monitor", 1);
+                orderProcessor.AddOrder("Keyboard", 1);
+                orderProcessor.ProcessOrder();
+                database.SaveOrder(customer, orderProcessor);
+
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Error saving order: "+ ex.Message);
+                Console.WriteLine("Error: " + ex.Message);
             }
         }
 
 
         class Customer
         {
-            public string customerName;
-            public string customerAddress;
+            public string customerName { get; set; }
+            public string customerAddress { get; set; }
 
-            public void GetCustomerDetails(string name, string address)
+            public void CustomerDetails(string name, string address)
             {
                 customerName = name;
                 customerAddress = address;
+
+                if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(address))
+                {
+                    throw new ArgumentException("Invalid customer details.");
+                }
+
+                Console.WriteLine($"Order for : {customerName} at {customerAddress}.");
             }
         }
         class OrderProcessor
         {
             private Dictionary<string, double> productPrices = new Dictionary<string, double>();
+            public Dictionary<string, int> savedOrders = new Dictionary<string, int>();
+
+            public List<int> quantities = new List<int>();
+            public List<string> productNames = new List<string>();
+
             public OrderProcessor()
             {
 
@@ -65,16 +72,15 @@ namespace CodeSmellsCodeExercise
                 productPrices["Monitor"] = 200;
                 productPrices["Keyboard"] = 50;
             }
-            public List<string> productNames = new List<string>();
-            public List<int> quantities = new List<int>();
-
             public void AddOrder(string productName, int quantity)
             {
                 productNames.Add(productName);
                 quantities.Add(quantity);
             }
-            public double ProcessOrder()
+
+            public void ProcessOrder()
             {
+
                 double totalPrice = 0;
 
                 for (int i = 0; i < productNames.Count; i++)
@@ -82,13 +88,35 @@ namespace CodeSmellsCodeExercise
                     if (!productPrices.ContainsKey(productNames[i]))
                     {
                         Console.WriteLine($"Unknown product: {productNames[i]}");
-                        //throw new ArgumentException($"Unknow product {productNames[i]}");
-
                         continue;
                     }
 
+                    savedOrders.Add(productNames[i], quantities[i]);
+
                     double itemPrice = productPrices[productNames[i]] * quantities[i];
                     totalPrice += itemPrice;
+                }
+
+                if (totalPrice <= 0)
+                {
+                    throw new ArgumentException("Invalid order details.");
+                }
+                
+                totalPrice = Discount(totalPrice);
+
+                Console.WriteLine($"Total price: {totalPrice}");
+
+            }
+
+            public double Discount(double totalPrice)
+            {
+                if (totalPrice > 1000 && totalPrice < 2000)
+                {
+                    return totalPrice * 0.90;
+                }
+                else if (totalPrice > 2000)
+                {
+                    return totalPrice * 0.85;
                 }
                 return totalPrice;
             }
@@ -98,60 +126,29 @@ namespace CodeSmellsCodeExercise
                 productNames.Clear();
                 quantities.Clear();
             }
-        }
-        class DiscountPrice
-        {
-            public double discountedPrice;
-            public double GetDiscount(double price)
-            {
-                discountedPrice = price;
-                if (discountedPrice > 1000)
-                {
-                    return discountedPrice *= 0.85;
-                }
-                else if (discountedPrice > 2000)
-                {
-                    return discountedPrice *= 0.90;
-                }
-                return price;
-            }
-        }
-        class Validation
-        {
-            public void ValidateOrder(double total)
-            {
-                if (total <= 0)
-                {
-                    throw new ArgumentException("Invalid order details.");
-                }
-            }
 
-            public void ValidateCustomerDetails(string name, string address)
-            {
-                if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(address))
-                {
-                    throw new ArgumentException("Invalid customer details.");
-                }
-            }
+           
         }
+
         class Database
         {
             public void SaveOrder(Customer customer, OrderProcessor orders)
             {
+                
+                Console.WriteLine($"Customer details: {customer.customerName} {customer.customerAddress}\n");
+                foreach (var order in orders.savedOrders)
+                {
+                    Console.WriteLine($"Product: {order.Key}, Quantity: {order.Value}");
+                }
 
                 Console.WriteLine("Order saved to database.\n");
-                orders.ClearOrders();
+                
+                orders.productNames.Clear();
+                orders.quantities.Clear();
+                
             }
         }
-        class DisplayInfo
-        {
-            public void display(Customer customer, double price)
-            {
-                Console.WriteLine($"Order for {customer.customerName} at {customer.customerAddress} processed. Total: {price}");
-            }
 
-        }
     }
 }
-
 
